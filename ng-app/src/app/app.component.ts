f3afa5743da26f4495d364252c574aabc8d46dd9
sakesun <sakesun@gmail.com>
2023-10-11 20:56:25 +0700

    remove service, put query in component

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -3,6 +3,21 @@ import { AllRocketsGQL, AllRocketsQuery } from '../generated/graphql'
 import { ApolloError } from '@apollo/client';
 import { GraphQLError } from 'graphql';
 import { ChangeDetectionStrategy } from '@angular/core';
+import { gql } from 'graphql-tag';
+
+let AllRockets = gql`
+  query AllRockets {
+    rockets {
+      special_id: id
+      type company boosters cost_per_launch country description
+      diameter { feet meters }
+      engines {
+        engine_loss_max layout number propellant_1 propellant_2
+        thrust_sea_level { kN lbf }
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
