8a1436d47ab083a82a8f74f51987e62e5b7f2e4e
sakesun <sakesun@gmail.com>
2023-10-11 20:39:35 +0700

    fetch and show result

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllRocketsGQL, AllRocketsQuery } from '../generated/graphql'
+import { ApolloError } from '@apollo/client';
+import { GraphQLError } from 'graphql';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-app';
+  loading = false;
+  apolloError?: ApolloError;
+  graphQLError?: readonly GraphQLError[];
+  rockets: AllRocketsQuery['rockets'];
+  public constructor(private allRockets: AllRocketsGQL) {}
+  ngOnInit(): void {
+    this.allRockets.fetch().subscribe(e => {
+      this.loading = e.loading;
+      this.apolloError = e.error;
+      this.graphQLError = e.errors;
+      this.rockets = e.data.rockets;
+    })
+  }
 }
--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { InMemoryCache } from '@apollo/client/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular'
+import { HttpLink } from 'apollo-angular/http'
 import { AppComponent } from './app.component';
+
+const SPACEX_PROVIDER = {
+  provide: APOLLO_OPTIONS,
+  useFactory(httpLink: HttpLink) {
+    return {
+      cache: new InMemoryCache(),
+      link: httpLink.create({
+        uri: "https://spacex-production.up.railway.app/"
+      })
+    }
+  },
+  deps: [HttpLink]
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    SPACEX_PROVIDER
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
